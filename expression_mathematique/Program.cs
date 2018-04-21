368d12d3f36926be651749d7023eb13491508b8f
Maxime Cohen <maxime.cohen2@epsi.fr>
2018-04-21 11:45:55 +0200

    add expression mathematique

---
+++ expression_mathematique/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace expression_mathematique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
